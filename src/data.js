ec69393ff8b75209fb40dde73565635f10d0420d
Haitham <haithhawk@gmail.com>
2018-04-06 21:33:48 +0300

    adding the map with the autocomplete

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const stores = [
+  {
+    Address: 'Haifa Center - HaShmona, Haifa, Israel',
+    id: 1,
+    coordinates: { lat: 32.7929, lng: 35.0343 },
+    Store_Name: 'Mega'
+  },
+  {
+    Address: 'Haifa Bay Central Bus Station',
+    id: 2,
+    coordinates: { lat: 32.0852999, lng: 34.78176759999997 },
+    Store_Name: 'Mega'
+  },
+
+  {
+    Address: 'TA University',
+    id: 3,
+    coordinates: { lat: 32.7929, lng: 35.0343 },
+    Store_Name: 'Rami Levy '
+  },
+
+  {
+    Address: 'Tel Aviv - Savidor Center,',
+    id: 4,
+    Store_Name: 'Rami Levy '
+  }
+];
+
+export default stores;
